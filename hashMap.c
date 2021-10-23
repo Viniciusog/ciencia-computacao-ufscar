f6a609b75e57812f13be348d10e12c0fd3b4af0e
viniciusog <vinijipa23@gmail.com>
2021-10-23 16:23:12 -0400

    hash map structs

---
+++ hashMap.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+//strcpy(a[i].chave, "Olá");
+
+#define N 5
+
+
+typedef struct hashMap {
+    double valor;
+    char chave[30];
+} HashMap;
+
+void lerValoresHashMap(HashMap a[N]) {
+    int i;
+    for (i = 0; i < N; i++) {
+        scanf("%s %lf", a[i].chave, &a[i].valor);
+    }
+}
+
+void printarValoresHashMap(HashMap a[N]) {
+    int i;
+    printf("\n[\n");
+    for (i = 0; i< N; i++) {
+        printf(" {\n");
+        printf("  Chave: %s,\n", a[i].chave);
+        printf("  Valor: %lf,\n", a[i].valor);
+        printf(" }");
+        if (i != N - 1) {
+            printf(",\n");
+        }
+    }
+    printf("\n]\n");
+}
+
+double pegarValorPorChave(HashMap a[N], char chave[N]) {
+    int i;
+    for (i = 0; i < N; i++) {
+        if (strcmp(a[i].chave, chave) == 0) {
+            return a[i].valor;
+        }
+    } 
+}
+
+
+int main(int argc, char const *argv[])
+{
+    HashMap myHashMap[N];
+    
+    lerValoresHashMap(myHashMap);
+    printarValoresHashMap(myHashMap);
+
+
+    double valor = pegarValorPorChave(myHashMap, "Uva");
+    printf("\n O valor encontrado com a chave 'Uva' é: %lf", valor);
+
+    return 0;
+}
