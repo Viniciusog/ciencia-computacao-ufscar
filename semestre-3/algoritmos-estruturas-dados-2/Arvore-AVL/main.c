1c9f19308413070b8c58a95ef91787661a452cbf
viniciusog <vinijipa23@gmail.com>
2022-07-07 18:16:16 -0400

    update main

--- semestre-3/algoritmos-estruturas-dados-2/Arvore-AVL/main.c
+++ semestre-3/algoritmos-estruturas-dados-2/Arvore-AVL/main.c
@@ -2,63 +2,6 @@
 #include <stdio.h>
 #include "Tree.c"
 int main() {
-    Node *node = create();
-    /* node = insert(node, 50);
-    node = insert(node, 20);
-    node = insert(node, 18);
-    node = insert(node, 64);
-    node = insert(node, 27); 
-    
-    print(node);
-    printf("\n");
-    node = rightRotation(node);
-    print(node);
-    */
-
-   /* 
-    node = insert(node, 50);
-    node = insert(node, 25);
-    node = insert(node, 64);
-    node = insert(node, 58);
-    node = insert(node, 90);
-
-    print(node);
-    printf("\n");
-    node = leftRotation(node); 
-    
-   */
-
-    // TEST LEFT RIGHT ROTATION
-    /* node = insert(node, 50);
-    node = insert(node, 30);
-    node = insert(node, 20);
-    node = insert(node, 40);
-    node = insert(node, 35);
-    node = insert(node, 45);
-    node = insert(node, 60);
-    node = insert(node, 70);
-
-    print(node);
-    printf("\n");
-    node = leftRightRotation(node);
-    print(node); */
-
-    //TESTE LEFT RIGHT ROTATION
-    /*
-    node = insert(node, 50, 111);
-    node = insert(node, 35, 112);
-    node = insert(node, 70, 113);
-    node = insert(node, 60, 114);
-    node = insert(node, 95, 115);
-    node = insert(node, 54, 116);
-    node = insert(node, 69, 117);
-
-    print(node);
-    printf("\n");
-    node = rightLeftRotation(node);
-    print(node); 
-    */
-
     Node *tree = create();
     int heightIncreased;
     tree = insertAVL(tree, 50, 111, &heightIncreased);
