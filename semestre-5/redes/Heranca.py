0b87d1d9ed105cc4b30872b2fe17d7935ceb323b
viniciusog <viniciusogbr@gmail.com>
2023-05-22 16:46:51 -0300

    Python classmethod

--- semestre-5/redes/Heranca.py
+++ semestre-5/redes/Heranca.py
@@ -2,6 +2,7 @@ class Pet:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        print("pet constructor")
 
     def printPet(self):
         print("Name: " + self.name + ", age: " + str(self.age))
@@ -10,7 +11,8 @@ class Pet:
         print("Show Pet")
 
 class Dog(Pet):
-    def __init__(self, attDog):
+    def __init__(self, name, age, attDog):
+        super().__init__(name, age)
         self.attDog = attDog
     
     def show(self):
@@ -24,7 +26,7 @@ class Cat(Pet):
         self.attCat = kwargs.get('attCat', -1)
     
     def show(self):
-        print("Show cat")
+        super().show()
 
 myPet = Pet("toto", 6)
 myPet.printPet()
@@ -34,6 +36,71 @@ myPet.show()
 myCat = Cat("lili", 2, 37, 38, 39, attCat = 8)
 print("\n --cat--\n" + str(myCat.attCat))
 print(myCat.anotherAttributes) # (37, )
+myCat.show()
 
-class Fish():
+print("\n--dog--")
+myDog = Dog("haka", 2, -4)
+myDog.printPet()
+print(myDog.attDog)
+
+class Fish(Pet):
+    quantidade_peixe = 0 # atributo estático 
+    
+    def __init__(self, name, age, cor):
+        super().__init__(name, age)
+        self.cor = cor
+        Fish.quantidade_peixe += 1
+
+    @classmethod #método da classe
+    def number_of_fishes(cls):
+        return cls.quantidade_peixe
+    
+    @staticmethod
+    def static_print():
+        print("printando peixe estático")
+
+    def print_normal(self):
+        print("printando peixe normal")
+
+print("\n")
+fish = Fish("nemo", 1, "orange")
+fish.printPet()
+print("Quantidade de peixes: " + str(fish.quantidade_peixe))
+
+print("\n")
+fish2 = Fish("teste", 4, "red")
+fish2.printPet()
+print("Quantidade de peixes: " + str(fish2.quantidade_peixe))
+
+qtd = Fish.number_of_fishes()
+print("Qtd classmethod: " + str(qtd))
+Fish.static_print()
+
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+    
+    def falar(self, frase):
+        print("Falando: " + frase)
+
+    @classmethod #Aqui está servindo como um método factory (para criar objetos, é possível herdar)
+    def criar_pessoa_com_ano_nascimento(cls, nome, ano_nascimento):
+        return cls(nome, 2023 - ano_nascimento)
+    
+    def imprimir(self):
+        print("Nome: " + self.nome + ", idade: " + str(self.idade))
+    
+class Professor(Pessoa):
     pass
+
+print("\n --pessoa--")
+p = Pessoa("fernando", 20)
+p.imprimir()
+
+p2 = Pessoa.criar_pessoa_com_ano_nascimento("maria", 2001)
+p2.imprimir()
+
+p3 = Professor.criar_pessoa_com_ano_nascimento("pedro", 1990)
+p3.imprimir()
